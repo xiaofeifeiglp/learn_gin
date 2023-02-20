29e5e39369765314dd7650eeb6470f5f93de5f35
wangfei <1010676900@qq.com>
2023-02-20 22:03:40 +0800

    go框架学习

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-    fmt.Println("Google" + "Runoob")
+	r := gin.Default()
+	// 定义一个路径为 /ping 的 GET 格式路由，并返回 JSON 数据
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, Gin by quanxiaoha.com !",
+		})
+	})
+	fmt.Printf("2r")
+	r.Run(":5658") // 启动服务，并监听 8080 端口
 }
